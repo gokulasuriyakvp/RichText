feb6f7c640f6cfab2651e377762b0e38b09b1efa
gokulasuriyakvp <134067562+gokulasuriyakvp@users.noreply.github.com>
2023-10-28 16:08:43 +0530

    initialcommit

---
+++ android/app/src/main/kotlin/com/example/flutter_page_view_rich_text/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_page_view_rich_text
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_page_view_rich_text/page_view_rich_text.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: PageViewRichText(),
+    );
+  }
+}
